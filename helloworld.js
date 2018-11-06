9b076457066ddf6fe7d48614f1285185bcf356d4
Chirag Furia <chiragfuria@gmail.com>
2018-11-06 10:22:06 -0500

    modified for JS

---
+++ helloworld.js
@@ -0,0 +1,17 @@
+function hc() {
+  console.log("Hello, World!");
+}
+
+function hp() {
+  alert("Hello, World!");
+}
+
+function hb() {
+  var p = document.getElementById('one');
+  p.innerHTML = "Hello, World!";
+}
+
+function hs() {
+  var stuff = prompt("Please enter your name:")
+  alert("Hello " + stuff);
+}
